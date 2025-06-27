c4ccea28b00deacba247408f65f850fd4ff70ea6
Nicholas Chiang <nicholas.chiang@numbersstation.ai>
2025-06-27 16:38:15 -0600

    feat: add script to install web apps
    I also ran it, and tracked those files. Likely shouldn't have. But it
    doesn't hurt and I want to stay consistent in tracking all of the
    `.desktop` shortcuts in `.local/share/applications`. I manually tweak
    them quite a bit.

---
+++ dot_local/share/private_applications/install-webapps.sh
@@ -0,0 +1,9 @@
+# Requires web2app to be on $PATH. It is included in `~/.local/bin`.
+# Inspired by DHH's Omarchy: https://github.com/basecamp/omarchy
+web2app "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
+web2app "Google Photos" https://photos.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-photos.png
+web2app "Google Contacts" https://contacts.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-contacts.png
+web2app "ChatGPT" https://chatgpt.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
+web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
+web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+web2app "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png

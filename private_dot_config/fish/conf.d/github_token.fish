122058ef74a2209fbfb2d41b789e66bcae7dcebb
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2025-12-08 22:18:25 -0700

    feat: add script to create Jira tickets
    Ref: https://github.com/ankitpokhrel/jira-cli?tab=readme-ov-file

---
+++ private_dot_config/fish/conf.d/github_token.fish
@@ -0,0 +1,3 @@
+# GitHub classic private access token for GitHub NPM registry
+# https://alation.slack.com/archives/C03JM9R0X0V/p1754367956598509
+set -gx GITHUB_TOKEN "ghp_***************************"
